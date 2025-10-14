07c51f0108a7e27e1498034202a33ddbdcbf692b
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2025-10-14 16:03:01 -0300

    Create
    criação do projeto

---
+++ src/main/java/com/example/poo2/Poo2Application.java
@@ -0,0 +1,13 @@
+package com.example.poo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poo2Application.class, args);
+	}
+
+}
