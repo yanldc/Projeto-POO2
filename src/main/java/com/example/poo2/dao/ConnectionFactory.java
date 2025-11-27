0efe58f544372a987509a06f323c210b85085c1c
Yan Lemke de Castro <yan.castro@compass.uol>
2025-11-27 10:14:10 -0300

    Update ConnectionFactory.java

--- src/main/java/com/example/poo2/dao/ConnectionFactory.java
+++ src/main/java/com/example/poo2/dao/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     private static final String URL = "jdbc:mysql://localhost:3306/dashboard_populacional";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "admin";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
